9bf0971de55e2e19abea4d23a3dd1b82c115c111
OverComms-Dev <Joshua.Wyatt@jhuapl.edu>
2021-02-07 12:46:35 -0500

    Added Second Model

---
+++ src/josh_model_one.py
@@ -0,0 +1,93 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+np.set_printoptions(linewidth = 200)
+
+(x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train_normalized = x_train / 255
+x_test_normalized = x_test / 255
+
+def plot_curve(epochs, hist, list_of_metrics):
+  """Plot a curve of one or more classification metrics vs. epoch."""  
+  
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Value")
+
+  for m in list_of_metrics:
+    x = hist[m]
+    plt.plot(epochs[1:], x[1:], label=m)
+
+  plt.legend()
+
+print("Loaded the plot_curve function.")
+
+def create_model(my_learning_rate):
+  """Create and compile a deep neural net."""
+  
+  # All models in this course are sequential.
+  model = tf.keras.models.Sequential()
+
+  model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+
+#first layer
+  model.add(tf.keras.layers.Dense(units= 65, activation= 'relu'))
+   
+#second layer
+  model.add(tf.keras.layers.Dense(units= 35 , activation = 'relu'))
+
+#third layer
+  model.add(tf.keras.layers.Dense(units= 27 , activation = 'relu'))
+
+
+  model.add(tf.keras.layers.Dropout(rate=0.2))
+
+
+  model.add(tf.keras.layers.Dense(units=10, activation='softmax'))     
+                             
+  model.compile(optimizer=tf.keras.optimizers.Adam(lr=my_learning_rate),
+                loss="sparse_categorical_crossentropy",
+                metrics=['accuracy'])
+  
+  return model    
+
+
+def train_model(model, train_features, train_label, epochs,
+                batch_size=None, validation_split=0.1):
+  """Train the model by feeding it data."""
+
+  history = model.fit(x=train_features, y=train_label, batch_size=batch_size,
+                      epochs=epochs, shuffle=True, 
+                      validation_split=validation_split)
+  epochs = history.epoch
+  hist = pd.DataFrame(history.history)
+
+  return epochs, hist  
+
+  # The following variables are the hyperparameters.
+learning_rate = 0.005
+epochs = 50
+batch_size = 4000
+validation_split = 0.25
+
+my_model = create_model(learning_rate)
+
+epochs, hist = train_model(my_model, x_train_normalized, y_train, 
+                           epochs, batch_size, validation_split)
+
+list_of_metrics_to_plot = ['accuracy']
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+
+# Evaluate against the test set.
+print("\n Evaluate the new model against the test set:")
+my_model.evaluate(x=x_test_normalized, y=y_test, batch_size=batch_size)
