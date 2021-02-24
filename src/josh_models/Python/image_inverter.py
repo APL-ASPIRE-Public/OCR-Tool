9d3e3fa2004f03979c5929976aebc4f9fe846b85
OverComms-Dev <Joshua.Wyatt@jhuapl.edu>
2021-02-24 15:58:40 -0500

    Added comments and corrected code

--- src/josh_models/Python/image_inverter.py
+++ src/josh_models/Python/image_inverter.py
@@ -1,7 +1,9 @@
 from PIL import Image, ImageOps
+#Needs user input
 
 def invert(image):     
   im = Image.open(image)
   im_invert = ImageOps.invert(im)
+  #outputs with a quality that is 95% of the original
   im_invert.save('/Users/wyattja1/Downloads/digit_one_invert.jpg', quality=95)
 invert('/Users/wyattja1/Desktop/OCR-Tool/src/josh_models/Handwritten_Digits/digit_one.jpg')
